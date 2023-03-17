a627c93b77e747fd2f53b28882813e3dd05e8548
Andrew Ferreira <andrew.shien2@gmail.com>
2023-03-17 11:18:53 -0300

    fix: corrige erro que impedia recarregamento usando `F5`

--- electron/menu/menu.ts
+++ electron/menu/menu.ts
@@ -1,10 +1,11 @@
 import { Menu } from 'electron';
-import { storeToRefs } from 'mechanus';
+import { computed, storeToRefs } from 'mechanus';
 import { useBrowserViewStore } from '$interface/index';
 import { showAppSettings } from '$electron/app/modules';
 import { togglePanelWindow, getMainWindow, getPanelWindow } from '$electron/utils/helpers';
 import { setBrowserDevMenu, setPanelDevMenu } from '$electron/menu/dev';
-import type { MenuItemConstructorOptions } from 'electron';
+import { getMainViewWebContents } from '$electron/utils/view';
+import type { MenuItemConstructorOptions, WebContents } from 'electron';
 
 // F1 - DevTools (apenas dev).
 // F2 - Exibe ou oculta o painel.
@@ -16,13 +17,17 @@ export function setAppMenu() {
     const panelWindow = getPanelWindow();
 
     const browserViewStore = useBrowserViewStore();
-    const { currentWebContents } = storeToRefs(browserViewStore);
+    const { currentWebContents: currentWebContentsMaybeNull } = storeToRefs(browserViewStore);
+
+    const currentWebContents = computed<WebContents>([currentWebContentsMaybeNull], () => {
+        return currentWebContentsMaybeNull.value ?? getMainViewWebContents();
+    });
 
     const sharedOptions: MenuItemConstructorOptions[] = [
         { label: 'Início', accelerator: 'CmdOrCtrl+Home' },
         { label: 'Exibir ou ocultar painel', accelerator: 'F2', click: () => togglePanelWindow() },
         { label: 'Configurações', accelerator: 'F3', click: () => showAppSettings('general-config') },
-        { label: 'Atualizar', accelerator: 'F5', click: () => currentWebContents.value?.reload() },
+        { label: 'Atualizar', accelerator: 'F5', click: () => currentWebContents.value.reload() },
         { label: 'Sair', accelerator: 'Esc', role: 'quit' }
     ];
 
