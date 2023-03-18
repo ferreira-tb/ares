e2cf56a2994e7feb1bbe80ae24b2b231f00dfd80
Andrew Ferreira <andrew.shien2@gmail.com>
2023-03-18 10:37:11 -0300

    fix: filtra `null` mais explicitamente

--- panel/utils/helpers.ts
+++ panel/utils/helpers.ts
@@ -3,7 +3,7 @@ import { routeNames, router } from '$panel/router/router';
 import type { useAresStore } from '$vue/stores/ares';
 
 export function pushRoute(screenName: ReturnType<typeof useAresStore>['screen']) {
-    if (arrayIncludes(routeNames, screenName)) {
+    if (screenName && arrayIncludes(routeNames, screenName)) {
         router.push({ name: screenName });
     } else {
         router.push('/');
