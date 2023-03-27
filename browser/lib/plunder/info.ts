5988396423f7b4b50276ba2b8cd96e15ef945516
Andrew Ferreira <andrew.shien2@gmail.com>
2023-03-27 15:07:27 -0300

    refactor: remove type guard desnecessário

--- browser/lib/plunder/info.ts
+++ browser/lib/plunder/info.ts
@@ -1,4 +1,3 @@
-import { assertObject } from '@tb-dev/ts-guard';
 import { Deimos } from '$deimos/shared/ipc';
 import { PlunderError } from '$browser/error';
 import { usePlunderStore } from '$vue/stores/plunder';
@@ -7,8 +6,8 @@ import { ipcSend } from '$global/ipc';
 export async function getPlunderInfo() {
     try {
         const plunderInfo = await Deimos.invoke('get-plunder-info');
-        assertObject(plunderInfo, 'Não foi possível obter as informações do assistente de saque.');
-
+        if (!plunderInfo) throw new PlunderError('Não foi possível obter os dados do assistente de saque.');
+        
         const plunderStore = usePlunderStore();
         plunderStore.$patch(plunderInfo);
         
