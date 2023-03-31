f28ac1daec86ff7f63049e81d87e7f9d4b5f9cc8
Andrew Ferreira <andrew.shien2@gmail.com>
2023-03-31 03:00:31 -0300

    refactor: move `updateGameData` para arquivo separado

--- browser/events/index.ts
+++ browser/events/index.ts
@@ -1,40 +1,11 @@
 import { ipcRenderer } from 'electron';
-import { Deimos } from '$deimos/shared/ipc';
-import { useAresStore } from '$global/stores/ares';
-import { useFeaturesStore } from '$global/stores/features';
-import { useGroupsStore } from '$global/stores/groups';
-import { usePlayerStore } from '$global/stores/player';
-import { useCurrentVillageStore } from '$global/stores/village';
-import { ipcSend } from '$global/ipc';
-import { BrowserError } from '$browser/error';
 import { setDevEvents } from '$browser/events/dev';
 import { setPlunderEvents } from '$browser/events/plunder';
+import { updateGameData } from '$browser/utils/helpers';
 import type { Pinia } from 'pinia';
 
 export function setBrowserEvents(pinia: Pinia) {
-    // Pinia.
-    const aresStore = useAresStore(pinia);
-    const featuresStore = useFeaturesStore(pinia);
-    const groupsStore = useGroupsStore(pinia);
-    const playerStore = usePlayerStore(pinia);
-    const currentVillageStore = useCurrentVillageStore(pinia);
-    
-    ipcRenderer.on('get-game-data', async () => {
-        try {
-            const gameData = await Deimos.invoke('get-game-data');
-            if (!gameData) return;
-
-            ipcSend('update-game-data', gameData);
-            aresStore.$patch(gameData.ares);
-            featuresStore.$patch(gameData.features);
-            groupsStore.$patch(gameData.groups);
-            playerStore.$patch(gameData.player);
-            currentVillageStore.$patch(gameData.currentVillage);
-            
-        } catch (err) {
-            BrowserError.catch(err);
-        };
-    });
+    ipcRenderer.on('get-game-data', () => updateGameData());
 
     // Outros eventos.
     setDevEvents();
