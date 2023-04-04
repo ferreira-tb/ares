7db84fbb81032468f68b9d95f66b5178f4f4910c
Andrew Ferreira <andrew.shien2@gmail.com>
2023-04-03 21:13:20 -0300

    feat: adiciona notificação de atualização ao menu principal

---
+++ main/components/TheResponseTime.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { useIpcRendererOn } from '@vueuse/electron';
+import { NTag } from 'naive-ui';
+import { ipcInvoke } from '$global/ipc';
+
+const responseTime = ref<number | null>(null);
+responseTime.value = await ipcInvoke('get-response-time');
+
+const responseTimeTagType = computed(() => {
+    if (responseTime.value) {
+        if (responseTime.value <= 250) return 'success';
+        if (responseTime.value <= 1000) return 'warning';
+    };
+
+    return 'error';
+});
+
+useIpcRendererOn('response-time-did-update', (_e, time: number) => {
+    responseTime.value = time;
+});
+</script>
+
+<template>
+    <div class="response-time-tag">
+        <Transition name="tb-fade" mode="out-in">
+            <div v-if="responseTime" class="tag-wrapper" :key="responseTimeTagType">
+                <NTag round :type="responseTimeTagType" size="small">
+                    {{ `${responseTime}ms` }}
+                </NTag>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.response-time-tag {
+    width: max-content;
+    height: 100%;
+
+    .tag-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        
+        width: inherit;
+        height: inherit;
+        padding-right: 0.5rem;
+    }
+}
+</style>
---
+++ main/components/TheUpdateNotification.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import semverGt from 'semver/functions/gt';
+import { computed, watchEffect } from 'vue';
+import { useFetch } from '@vueuse/core';
+import { NTag } from 'naive-ui';
+import { ipcInvoke } from '$global/ipc';
+
+type LatestVersion = {
+    readonly version: string;
+    readonly notes: string;
+    readonly download: string;
+};
+
+const appVersion = await ipcInvoke('app-version');
+const { data } = useFetch('https://tb.dev.br/ares/latest.json').json<LatestVersion>();
+
+const updateAvailable = computed(() => {
+    if (!data.value) return false;
+    return semverGt(data.value.version, appVersion);
+});
+
+watchEffect(() => {
+    console.log(data.value);
+    if (updateAvailable.value) {
+        
+    };
+});
+</script>
+
+<template>
+    <div class="update-notification-tag">
+        <Transition name="tb-fade" mode="out-in">
+            <div v-if="data && updateAvailable" class="tag-wrapper" :key="data.version">
+                <NTag round type="success" size="small" class="version-tag">
+                    Nova versão disponível
+                </NTag>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.update-notification-tag {
+    width: max-content;
+    height: 100%;
+
+    .tag-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        
+        width: inherit;
+        height: inherit;
+        padding-right: 0.5rem;
+        
+        .version-tag {
+            cursor: pointer;
+        }
+    }
+}
+</style>
