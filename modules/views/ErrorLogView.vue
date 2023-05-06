7210471ac1a01b26649ac830f5666eaa7c0c5e49
Andrew Ferreira <andrew.shien2@gmail.com>
2023-05-06 17:18:05 -0300

    feat: adiciona botão para exportar log de erros

--- modules/views/ErrorLogView.vue
+++ modules/views/ErrorLogView.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { RouterView } from 'vue-router';
-import { NTabs, NTab } from 'naive-ui';
+import { useElementSize, useWindowSize } from '@vueuse/core';
+import { NButton, NTabs, NTab } from 'naive-ui';
 import { router } from '$modules/router';
 import { ModuleRouterError } from '$modules/error';
 import type { ErrorModuleRoutes } from '$types/modules';
+
+const { height: windowHeight } = useWindowSize();
+
+const navBar = ref<HTMLElement | null>(null);
+const { height: navBarHeight } = useElementSize(navBar);
+
+const exportButton = ref<HTMLElement | null>(null);
+const { height: exportButtonHeight } = useElementSize(exportButton);
+
+const contentHeight = computed(() => windowHeight.value - navBarHeight.value);
+const buttonAreaTopPosition = computed(() => `${contentHeight.value - 30}px`);
+const wrapperHeight = computed(() => `${contentHeight.value - exportButtonHeight.value}px`);
 
 const route = ref<ErrorModuleRoutes>('error-general');
 watchEffect(() => {
@@ -13,14 +26,14 @@ watchEffect(() => {
 </script>
 
 <template>
-    <nav class="module-nav-bar">
+    <nav ref="navBar" class="module-nav-bar">
         <NTabs v-model:value="route" animated default-value="error-general" justify-content="start" tab-style="margin-right: 2em;">
             <NTab name="error-general" tab="Geral" />
             <NTab name="error-electron" tab="Electron" />
         </NTabs>
     </nav>
 
-    <div class="module-content">
+    <div class="module-tabbed-view-wrapper">
         <RouterView #default="{ Component }" class="module-view tb-scrollbar">
             <template v-if="Component">
                 <Transition name="tb-fade" mode="out-in">
@@ -36,15 +49,28 @@ watchEffect(() => {
             </template>
         </RouterView>
     </div>
+
+    <div ref="exportButton" class="btn-export-errors">
+        <NButton>Exportar</NButton>
+    </div>
 </template>
 
-<style scoped>
-:deep(.error-log) {
-    margin-bottom: 0.5em;
-    user-select: text;
+<style scoped lang="scss">
+@use '$modules/assets/main.scss';
+
+.module-tabbed-view-wrapper {
+    @include main.module-tabbed-view-wrapper($margin-bottom: 2rem);
+    bottom: v-bind("buttonAreaTopPosition");
+    height: v-bind("wrapperHeight");
 }
 
-:deep(.error-log:last-of-type) {
-    margin-bottom: 1em;
+.btn-export-errors {
+    position: absolute;
+    top: v-bind("buttonAreaTopPosition");
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
 }
 </style>
