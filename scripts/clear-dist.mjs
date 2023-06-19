7e37eff4dfa67ced4af62f88ca4deb242db95580
Andrew Ferreira <andrew.shien2@gmail.com>
2023-06-19 00:38:00 -0300

    build: organiza scripts

--- scripts/clear-dist.mjs
+++ scripts/clear-dist.mjs
@@ -6,15 +6,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 try {
-    const startTime = Date.now();
-
-    console.log('Limpando diretório de distribuição...');
-
     const distDir = path.resolve(__dirname, '../dist');
     await fs.rm(distDir, { recursive: true, force: true });
-
-    console.log(`Fim dos trabalhos. Tempo total: ${Date.now() - startTime}ms.`);
-
+    
 } catch (err) {
-    if (err instanceof Error) console.error(err);
+    console.error(err);
+    process.exit(1);
 };
---
+++ scripts/fix-files.mjs
@@ -0,0 +1,48 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+try {
+    const distDir = path.resolve(__dirname, '../dist');
+    const assetsDir = path.join(distDir, 'assets');
+
+    // HTML
+    const panel = path.join(distDir, 'panel.html');
+    const windows = path.join(distDir, 'windows.html');
+    const ui = path.join(distDir, 'ui.html');
+    await Promise.all([panel, windows, ui].map(html));
+
+    // CSS
+    const style = path.join(distDir, 'style.css');
+    const browser = path.join(distDir, 'browser.css');
+    await fs.rename(style, browser);    
+
+    // Fontes
+    const regex = /url\s*\(\/?fonts/g;
+    const replacement = 'url(../fonts';
+
+    const files = await fs.readdir(assetsDir);
+    const cssFiles = files.filter((file) => {
+        return path.extname(file) === '.css' && file.startsWith('ui');
+    });
+
+    for (const file of cssFiles) {
+        const cssFile = path.join(assetsDir, file);
+        let fileContent = await fs.readFile(cssFile, { encoding: 'utf-8' });
+        fileContent = fileContent.replace(regex, replacement);
+        await fs.writeFile(cssFile, fileContent, { encoding: 'utf-8' });
+    };
+    
+} catch (err) {
+    console.error(err);
+    process.exit(1);
+};
+
+async function html(file) {
+    let content = await fs.readFile(file, { encoding: 'utf-8' });
+    content = content.replace(/\"\/assets\//g, '\"assets\/');
+    await fs.writeFile(file, content, { encoding: 'utf-8' });
+};
